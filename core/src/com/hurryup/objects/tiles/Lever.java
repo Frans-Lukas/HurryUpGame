d17d2cd98621847c61e52749f1da9cfdbc7e911f
elklazor <uberhaxnovamegasuper@gmail.com>
2016-10-04 09:05:26 +0200

    Modified lever with "lever" and collision boxes

--- core/src/com/hurryup/objects/tiles/Lever.java
+++ core/src/com/hurryup/objects/tiles/Lever.java
@@ -15,23 +15,25 @@ public class Lever extends LogicTile {
     private final int baseHeight = 20;
     private Vector2 leverBasePosition;
     private boolean active = false;
-    private float leverMod = 0; //0-PI* 0,75
+    private int leverAngle = 40; //40 - - 40
 
     public Lever(Vector2 position) {
         super(position, LogicColor.Blue,2,0);
-        leverBasePosition = new Vector2(position.x + 30,position.y);
+        leverBasePosition = new Vector2(position.x + 25,position.y);
         height = 32;
+        state = 3;
     }
 
     @Override
     public void draw(SpriteBatch batch) {
         super.draw(batch);
 
-        renderer.setColor(Color.BROWN);
-        renderer.line(leverBasePosition,getLeverPos(leverMod).add(leverBasePosition));
+
+        renderer.setColor(Color.YELLOW);
+        renderer.rect(leverBasePosition.x,leverBasePosition.y + 15,5,0,10,35,1,1,leverAngle);
+
         renderer.setColor(buttonColor);
         renderer.rect(position.x + 10, position.y, width-20, baseHeight);
-
         if(connection[0] != null) {
             Vector2 tmpVector = new Vector2(vector.x + width / 2, vector.y + height / 2);
             renderer.setColor(Color.DARK_GRAY);
@@ -47,13 +49,20 @@ public class Lever extends LogicTile {
     public void update(long deltaTime) {
         super.update(deltaTime);
         //Active
-        if(state == 3 && leverMod < Math.PI/2){
-            leverMod += 0.1;
+        if(state == 3 && leverAngle > -40){
+            leverAngle--;
         }
         //Inactive
-        else if(state == 0 && leverMod > 0.1){
-            leverMod -= 0.1;
+        else if(state == 0 && leverAngle < 40){
+            leverAngle++;
         }
+    }
+
+    public void toggle(int whichToActivate){
+        if(state == 0)
+            activate(whichToActivate);
+        else if(state == 3)
+            deactivate(whichToActivate);
     }
 
     @Override
@@ -86,6 +95,41 @@ public class Lever extends LogicTile {
             state = 0;
         }
 
+    }
+
+    @Override
+    public Vector2 getPosition() {
+        return new Vector2(position.x + 10,position.y);
+    }
+
+    @Override
+    public float getLeft() {
+        return super.getLeft() + 10;
+    }
+
+    @Override
+    public float getRight() {
+        return super.getRight() -20;
+    }
+
+    @Override
+    public float getTop() {
+        return position.y + 20;
+    }
+
+    @Override
+    public float getBot() {
+        return super.getBot();
+    }
+
+    @Override
+    public float getHeight() {
+        return 20;
+    }
+
+    @Override
+    public float getWidth() {
+        return super.getWidth()-20;
     }
 
     public void setColor(Color color){
