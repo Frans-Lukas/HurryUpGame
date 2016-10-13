28ab846eb4d7d589aaafc1cf9196e37b80ba0d5e
elklazor <uberhaxnovamegasuper@gmail.com>
2016-10-13 20:39:37 +0200

    Changed getVector2 to no longer return the origin of the tile

--- core/src/com/hurryup/objects/tiles/LogicTile.java
+++ core/src/com/hurryup/objects/tiles/LogicTile.java
@@ -101,7 +101,7 @@ public class LogicTile extends Tile implements IInteractive{
 
     public Vector2 getVector2(){
         //plus one so straight lines will not turn invisible.
-        Vector2 tmpVector = new Vector2(vector.x + width / 2 + 1, vector.y + height / 2 + 1);
+        Vector2 tmpVector = new Vector2(vector.x, vector.y);
         return tmpVector;
     }
 
